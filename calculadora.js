02b569558b3a503ac2269afeb14c3cdb68e23f91
Ney33 <juanxj93@gmail.com>
2026-02-12 11:55:25 -0500

    errores de estado

--- calculadora.js
+++ calculadora.js
@@ -1,35 +1,58 @@
+  
+    //creado estado de error
+        let hayerror = false;
 
-    const form = document.forms["calculator"];
-    const display = form["resp"];
-    const buttons = form.querySelectorAll("input[type='button']");
+        const form = document.forms["calculator"];
+        const display = document.querySelector("input[name='resp']");
+        const buttons = document.querySelectorAll("input[type='button']");
 
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
+        buttons.forEach(button => {
+         button.addEventListener("click", () => {
+
             const value = button.value;
+
+            // bloqueo de imput si hay error
+            if (hayerror && value !== "ac") {
+                return;
+            }
 
             if (value === "=") {
                 calcular();
-            } else if (value === "ac") {
+            } 
+            else if (value === "ac") {
                 limpiar();
-            } else {
-                agregarValor(value);
+            } 
+            else {
+                display.value += value;
             }
         });
-    });
-
-    function agregarValor(valor) {
-        display.value += valor;
-    }
-
+     });
+ 
     function limpiar() {
         display.value = "";
+        hayerror = false;
     }
 
     function calcular() {
         try {
-            display.value = eval(display.value);
-        } catch (error) {
-            display.value = "Error";
+            let resultado = eval(display.value);
+
+    //correcion de resultado si el numero da infinito se pone en estado d error 
+            if (!isFinite(resultado)) {   
+                display.value = "error";
+                hayerror = true;
+                return;
+            }
+
+            display.value = resultado;
+
+        } catch (e) {
+            display.value = "error";
+            hayerror = true;
         }
     }
+
+        
+    
+    
 
